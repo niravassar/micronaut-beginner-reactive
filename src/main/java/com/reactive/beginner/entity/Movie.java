273bb30693e7150aff39d2c7af1ca28024208718
nassar <assarn@objectcomputing.com>
2023-01-30 14:12:01 -0600

    data service

--- src/main/java/com/reactive/beginner/entity/Movie.java
+++ src/main/java/com/reactive/beginner/entity/Movie.java
@@ -8,6 +8,11 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class Movie {
+
+    public static String ACTION = "action";
+    public static String COMEDY = "comedy";
+    public static String DRAMA = "drama";
+
     private String name;
     private Integer year;
     private String genre;
