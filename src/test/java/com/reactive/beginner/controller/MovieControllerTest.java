a951a8e915eaf220bcba4da5163a95316724152e
nassar <assarn@objectcomputing.com>
2023-02-01 10:02:06 -0600

    Argument list of

--- src/test/java/com/reactive/beginner/controller/MovieControllerTest.java
+++ src/test/java/com/reactive/beginner/controller/MovieControllerTest.java
@@ -3,6 +3,7 @@ package com.reactive.beginner.controller;
 import com.reactive.beginner.entity.Actor;
 import com.reactive.beginner.entity.Movie;
 import com.reactive.beginner.service.MovieService;
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
@@ -30,10 +31,10 @@ public class MovieControllerTest {
     @Test
     void test_blocking_actorsOlder64() {
         HttpRequest<?> request = HttpRequest.GET("/actorsOlder64");
-        HttpResponse<List> response = httpClient.toBlocking().exchange(request, List.class);
-        List<LinkedHashMap> actors = response.body();
+        HttpResponse<List<Actor>> response = httpClient.toBlocking().exchange(request, Argument.listOf(Actor.class));
+        List<Actor> actors = response.body();
         assertEquals(HttpStatus.OK, response.getStatus());
-        assertEquals("Harrison Ford", actors.get(0).get("name"));
-        assertEquals("Bill Murray", actors.get(1).get("name"));
+        assertEquals("Harrison Ford", actors.get(0).getName());
+        assertEquals("Bill Murray", actors.get(1).getName());
     }
 }
